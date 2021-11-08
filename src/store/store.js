66328132255ad80abd7d029c1884ea2f3507e49a
Manohar-Maharshi <netwitch101@gmail.com>
2021-11-08 10:52:08 +0530

    ➖removed unused dependencies/variables

--- src/store/store.js
+++
@@ -1,8 +0,0 @@
-import { writable } from 'svelte/store';
-
-
-
-export const webResults = writable({});
-export const imageResults = writable({});
-export const newsResults = writable({});
-export const videoResults = writable({});
